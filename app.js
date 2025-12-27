c21841a2d6a3a0686c46619dbbfbc84063b7809a
Naman7564 <Naman7564@gmail.com>
2025-12-27 11:13:05 +0530

    update

--- app.js
+++ app.js
@@ -21,68 +21,77 @@ app.use(methodOverride(function (req, res) {
 }));
 
 
-let todolist = [];
+// Notes array - each note has title and content
+let notes = [];
 
-/* The to do list and the form are displayed */
-app.get('/todo', function (req, res) {
-        res.render('todo.ejs', {
-            todolist,
-            clickHandler: "func1();"
-        });
-    })
+/* The notes list and the form are displayed */
+app.get('/notes', function (req, res) {
+    res.render('todo.ejs', {
+        notes,
+        clickHandler: "func1();"
+    });
+})
 
-    /* Adding an item to the to do list */
-    .post('/todo/add/', function (req, res) {
+    /* Adding a note to the notes list */
+    .post('/notes/add/', function (req, res) {
         // Escapes HTML special characters in attribute values as HTML entities
-        let newTodo = sanitizer.escape(req.body.newtodo);
-        if (req.body.newtodo != '') {
-            todolist.push(newTodo);
+        let newTitle = sanitizer.escape(req.body.newTitle);
+        let newContent = sanitizer.escape(req.body.newContent);
+        if (newTitle != '' || newContent != '') {
+            notes.push({
+                title: newTitle || 'Untitled',
+                content: newContent || ''
+            });
         }
-        res.redirect('/todo');
+        res.redirect('/notes');
     })
 
-    /* Deletes an item from the to do list */
-    .get('/todo/delete/:id', function (req, res) {
+    /* Deletes a note from the notes list */
+    .get('/notes/delete/:id', function (req, res) {
         if (req.params.id != '') {
-            todolist.splice(req.params.id, 1);
+            notes.splice(req.params.id, 1);
         }
-        res.redirect('/todo');
+        res.redirect('/notes');
     })
 
-    // Get a single todo item and render edit page
-    .get('/todo/:id', function (req, res) {
-        let todoIdx = req.params.id;
-        let todo = todolist[todoIdx];
+    // Get a single note and render edit page
+    .get('/notes/:id', function (req, res) {
+        let noteIdx = req.params.id;
+        let note = notes[noteIdx];
 
-        if (todo) {
+        if (note) {
             res.render('edititem.ejs', {
-                todoIdx,
-                todo,
+                noteIdx,
+                note,
                 clickHandler: "func1();"
             });
         } else {
-            res.redirect('/todo');
+            res.redirect('/notes');
         }
     })
 
-    // Edit item in the todo list 
-    .put('/todo/edit/:id', function (req, res) {
-        let todoIdx = req.params.id;
+    // Edit note in the notes list 
+    .put('/notes/edit/:id', function (req, res) {
+        let noteIdx = req.params.id;
         // Escapes HTML special characters in attribute values as HTML entities
-        let editTodo = sanitizer.escape(req.body.editTodo);
-        if (todoIdx != '' && editTodo != '') {
-            todolist[todoIdx] = editTodo;
+        let editTitle = sanitizer.escape(req.body.editTitle);
+        let editContent = sanitizer.escape(req.body.editContent);
+        if (noteIdx != '') {
+            notes[noteIdx] = {
+                title: editTitle || 'Untitled',
+                content: editContent || ''
+            };
         }
-        res.redirect('/todo');
+        res.redirect('/notes');
     })
-    /* Redirects to the to do list if the page requested is not found */
+    /* Redirects to the notes list if the page requested is not found */
     .use(function (req, res, next) {
-        res.redirect('/todo');
+        res.redirect('/notes');
     })
 
     .listen(port, function () {
         // Logging to console
-        console.log(`Todolist running on http://0.0.0.0:${port}`)
+        console.log(`Notes App running on http://0.0.0.0:${port}`)
     });
 // Export app
 module.exports = app;
